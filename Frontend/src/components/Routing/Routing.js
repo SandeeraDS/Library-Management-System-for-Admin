f4b0ac678bbd32e591d8c80708a285676fdf1ccc
sandeeraDS <sandeerads@gmail.com>
2018-12-13 18:33:54 +0530

    frontend basic completed

---
+++ Frontend/src/components/Routing/Routing.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Router, Route, browserHistory } from 'react-router';
+import Book from '../Books/Books';
+import OverDue from '../OverDue/OverDue';
+import Users from '../Users/Users';
+import Assign from '../Assign/Assign';
+import App from '../../containers/App'
+
+const Routing = () => {
+    return (
+
+        <Router history={browserHistory}>
+            <Route path="/" component={App}></Route>
+            <Route path="/Book" component={Book}></Route>
+            <Route path="/Overdue" component={OverDue}></Route>
+            <Route path="/Users" component={Users}></Route>
+            <Route path="/Assign" component={Assign}></Route>
+        </Router>
+
+    );
+}
+
+export default Routing;
+
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,36 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
-import {Router, Route, browserHistory } from 'react-router';
-import Book from './components/Books/Books';
-import OverDue from './components/OverDue/OverDue';
-import Users from './components/Users/Users';
-import Assign from './components/Assign/Assign';
+import Routing from './components/Routing/Routing';
 
-ReactDOM.render(
-    
-    <Router history={browserHistory}>
-    <Route path="/" component={App}></Route>
-    <Route path="/Book" component={Book}></Route>
-    <Route path="/Overdue" component={OverDue}></Route>
-    <Route path="/Users" component={Users}></Route>
-    <Route path="/Assign" component={Assign}></Route>
-    {/* <Route path="/One" component={One}></Route>
-    <Route path="/Two" component={Two}></Route>
-    <Route path="/Three" component={Three}></Route>
-    <Route path="/Four" component={Four}>
-    <Route path="/Four/:id" component={FourPointOne}></Route> 
-    </Route>
-    <Route path="*" component={NoMatch}></Route> */}
-
-    </Router>
-
-
-    
-
-    , document.getElementById('root'));
+ReactDOM.render(<Routing />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
